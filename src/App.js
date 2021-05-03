81e75a55c0614feaf3a1cb193b7212b08f8576d5
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-02 23:59:18 -0400

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
+import Nav from './copmonents/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
     <h1>Thank you for visiting my page!</h1>
+    
     </div>
   );
 }
---
+++ src/copmonents/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as ReactBootStrap from 'react-bootstrap';
+
+ function Nav() {
+    return (
+        <div className="Nav">
+        <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <ReactBootStrap.Navbar.Brand href="#home">Meqdad</ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
+            <ReactBootStrap.Nav.Link href="#my-reading-list">My Reading List</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#about-me">About Meqdad</ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
+      </div>
+    )
+}
+
+export default Nav; 
