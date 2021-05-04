5e687497fc55bf455df8eaeba5fe3b0b490b130b
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-04 15:02:41 -0400

    styled the header h1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import Nav from './components/Nav';
 import Countries from './components/Countries';
 import Filter from './components/Filter'
@@ -12,7 +13,7 @@ function App() {
   return (
     <>
       <Nav/>
-      <h1>Thank you for visiting my page!</h1>
+      <h1>Thank you for visiting my page! </h1>
       <Filter/>
       <Countries/>
       
