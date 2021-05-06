ce98459d03a1232efc9ab355c7b5a5f83743a5fe
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-06 13:03:49 -0400

    added to details component

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -20,12 +20,20 @@ const Countries = () => {
         useEffect (() => {
             getCountries()
     },[])
+// remove button in the countries descriptio form
+    const remove=(numericCode) => {
+        const newCountry = countries.filter((country)=>
+        country.numericCode !== numericCode)
+        setCountries(newCountry)
+    }
     return (
         <>
         <div className="grid">
+            {/* the countries component will map over the the data we pulled from API */}
         {countries.map((country) => {
                 const { numericCode, name, capital, population, region, flag } = country
                 return (
+                    // the Key should be a unique code in the API. numericCode is a very unique one for every individual country
                     <article key={numericCode}>
                         <div>
                         <img className="image" src={flag} alt={flag}/>
@@ -34,7 +42,12 @@ const Countries = () => {
                         <h5>Capital: {capital}</h5>
                         <h5>Population: {population}</h5>
                         <h5>Region: {region}</h5>
-                        <Link to ={`/${name}`}>More</Link>
+                       <div className="functions">
+                           {/* adding the router name and connect it to our country component so when clicked it will open a new router with more details of the countries */}
+                           <Link to ={`/${name}`}>More</Link>
+                           {/* adding an onClick event in our countries description form so when clicked should remove the selected country from the list */}
+                        <button className="remove" onClick={() =>
+                        remove(numericCode)}>Remove</button></div>
                         </div>
                         </div>
                     </article>
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {Link, useParams} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+
+
 
 const Details = () => {
+// setting the state value 
+const [details, setDetails] = useState ([])
+const {name} = useParams()
+
+useEffect (() => {
+    const countryData = async () => {
+        const response = await fetch (`https://restcountries.eu/rest/v2/name/${name}`)
+        const Details = await response.json()
+        setDetails(details)
+    }
+    countryData()
+},[])
+
     return (
         <>
+        <Link to ="/">
+            <i className="fas fa-arrow-left"></i>Back Home
+        </Link>
             <h1>Hello world</h1>
         </>
     )
